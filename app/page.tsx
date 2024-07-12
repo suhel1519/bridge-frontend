11ac10396cf9fca62697191ebb0e10e87c5740d8
suhel <116453294+suhel1519@users.noreply.github.com>
2024-07-12 10:32:02 +0530

    refacted

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import BridgeBox from "@/components/modals/bridgeBox";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center space-y-6 p-24">
-      <h1 className="text-white text-3xl">Bridge</h1>
-      <BridgeBox />
-      {/* <SearchBar label="From" /> */}
-      {/* <SearchBar label="To" /> */}
-    </main>
+    <Suspense>
+      <main className="flex min-h-screen flex-col items-center space-y-6 p-24">
+        <h1 className="text-white text-3xl">Bridge</h1>
+        <BridgeBox />
+      </main>
+    </Suspense>
   );
 }
